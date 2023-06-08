dc768aebb9f98d46ba4dc31bd02392ac66c3c210
42null <averyparanoiduser@gmail.com>
2023-06-07 22:20:27 -0500

    Bugfix: Dynamatic content path resolve

--- server/src/main/java/net/the42null/personalwebsite/helpers/ContentPather.java
+++ server/src/main/java/net/the42null/personalwebsite/helpers/ContentPather.java
@@ -14,13 +14,13 @@ public class ContentPather {
 	private final static String innerDefaultPath = "src/main/resources/static/content/";//TODO: Generate dynamically
 
 	public enum Target {
-		INTILLIJ_RUN("server/"+innerDefaultPath),
-		RASPBERRY_DIRECT;
+		RASPBERRY_DIRECT(innerDefaultPath),
+		INTILLIJ_RUN("server/"+innerDefaultPath);
 
 		private final String path;
 
 		Target(){
-			this.path = "";
+			this.path = null;
 		}
 		Target(String path){
 			this.path = path;
@@ -33,15 +33,13 @@ public class ContentPather {
 
 	private String buildPath;
 
-	public ContentPather(){//Check directories automaticallyc
+	public ContentPather(){//Check directories automatically
 		for(int i = 0; i < Target.values().length; i++){
 			Path directoryPath = Paths.get(Target.values()[i].getPath());
-			System.out.println("directoryPath = "+directoryPath);
 			if(Files.isDirectory(directoryPath)){
 				buildPath = Target.values()[i].getPath();
 				return;
 			}else{
-
 			}
 		}
 		throw new IllegalArgumentException("Auto-pick content directory failure, please provide a location or move files to a enum defined location");
